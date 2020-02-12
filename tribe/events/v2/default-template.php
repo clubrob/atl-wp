7b8a2d66445a845eb076fb14b5283ff165afd13e
Rob Lindsey <rob@roblindsey.com>
2020-02-12 17:31:16 -0500

    fixed events plugin template overrides 2

--- tribe/events/v2/default-template.php
+++ tribe/events/v2/default-template.php
@@ -1,6 +1,3 @@
-<div class="entry-header container single-column">
-  <h1 class="entry-title">Events</h1>
-</div>
 <?php
 /**
  * View: Default Template for Events
@@ -18,5 +15,17 @@
 use Tribe\Events\Views\V2\Template_Bootstrap;
 
 get_header();
-echo tribe( Template_Bootstrap::class )->get_view_html();
+?>
+<div class="page-thumbnail">
+	<img src="http://localhost:8080/wp-content/uploads/2019/08/events-banner-2560px.jpg" class="attachment-post-thumbnail size-post-thumbnail wp-post-image" alt="events" srcset="http://localhost:8080/wp-content/uploads/2019/08/events-banner-2560px.jpg 2560w, http://localhost:8080/wp-content/uploads/2019/08/events-banner-2560px-300x169.jpg 300w, http://localhost:8080/wp-content/uploads/2019/08/events-banner-2560px-768x432.jpg 768w, http://localhost:8080/wp-content/uploads/2019/08/events-banner-2560px-1024x576.jpg 1024w" sizes="(max-width: 2560px) 100vw, 2560px" width="2560" height="1440">			<div class="caption">
+		<h1>Events</h1>
+	</div>
+</div>
+<div class="single-column">
+	<?php
+	echo tribe( Template_Bootstrap::class )->get_view_html();
+	?>
+</div>
+
+<?php
 get_footer();
